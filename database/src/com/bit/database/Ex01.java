7d169986a5f49fcc95b65dfe6cb5522ecc7773e0
Kim-Tae-Kwan <vodjaos@gmail.com>
2022-04-11 17:14:12 +0900

    [ADD] 4/11 JDBC 내용 추가.

---
+++ database/src/com/bit/database/Ex01.java
@@ -0,0 +1,43 @@
+package com.bit.database;
+
+// 데이터베이스 연결.
+// insert, update, delete
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.Properties;
+
+import com.mysql.cj.jdbc.*;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/test01";
+		
+//		String sql = "insert into bbs01 values (7, 'test7', 'tester', 'test')";
+//		String sql = "update bbs01 set content='test' where num=6";
+		String sql = "delete from bbs01 where num=6";
+		
+		// IO&Map 동시
+		Properties info = new Properties();
+		info.setProperty("user", "user01");
+		info.setProperty("password", "1234");
+		
+		try {
+			Driver driver = new Driver();
+			Connection connection = DriverManager.getConnection(url, info);
+			
+			Statement statement = connection.createStatement();
+			statement.executeUpdate(sql);
+			
+			statement.close();
+			connection.close();
+			System.out.println("DML 수행 성공");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ database/src/com/bit/database/Ex02.java
@@ -0,0 +1,57 @@
+package com.bit.database;
+
+//select
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+import com.mysql.cj.jdbc.Driver;
+
+public class Ex02 {
+	public static void main(String[] args) {
+		String sql = "select num, sub, name, content from bbs01";
+		String url = "jdbc:mysql://localhost:3306/test01";
+		Properties info = new Properties();
+		info.setProperty("user", "user01");
+		info.setProperty("password", "1234");
+		
+		Driver driver = null;
+		Connection conn = null; // 데이터베이스 연결.
+		Statement stmt = null; // 쿼리 날리기 위한 인터페이스.
+		ResultSet rs = null; // select 결과 셋.
+		
+		try {
+			driver = new Driver();
+			conn = DriverManager.getConnection(url, info);
+			
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				System.out.print("num : " + rs.getString(1) + "\t");
+				System.out.print("sub : " + rs.getString(2) + "\t");
+				System.out.print("name : " + rs.getString(3) + "\t");
+				System.out.println("content : " + rs.getString(4) + "\t");
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(stmt != null) stmt.close();
+				if(conn != null) conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
+		
+	}
+
+}
