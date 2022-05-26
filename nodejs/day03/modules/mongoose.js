bc98e98eccfeb21d21483c1543c9d0ddf64d9f2f
Kim-Tae-Kwan <vodjaos@gmail.com>
2022-05-26 09:00:33 +0900

    [MOD] MongoDB connect log 변경.

--- nodejs/day03/modules/mongoose.js
+++ nodejs/day03/modules/mongoose.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/testDB');
 var db = mongoose.connection;
 db.on('error', function(){
-    console.log('Connection Failed!');
+    console.log('MongoDB Connection Failed!');
 });
 db.once('open', function() {
-    console.log('Connected!');
+    console.log('MongoDB Connected!');
 });
 
 var emp = mongoose.Schema({
